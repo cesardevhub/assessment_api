c3056f9b3412a3204257327116a52c83f6a343ee
César de León <cdeleon2205@outlook.com>
2026-05-18 10:29:37 -0600

    UPDATE: add trust proxy

--- src/config/appServer.ts
+++ src/config/appServer.ts
@@ -16,6 +16,7 @@ import cookieParser from 'cookie-parser'
 const app = express();
 const server = http.createServer(app)
 
+app.set('trust proxy', 1)
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cookieParser())
